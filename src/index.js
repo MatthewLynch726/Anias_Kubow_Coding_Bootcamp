f8ddb652473995d0532ec40a79b63a6271e42cfc
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-07-08 23:06:26 -0400

    Wrote the Fizz Buzz program

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 
 var x = 1
 
-x = 23
+while (x < 31){
+    
+    if(x % 3 == 0 && x % 5 == 0){
+        console.log("fiz buz")
+    }else if(x % 3 == 0){
+        console.log("fizz")
+    }else if(x % 5 == 0){
+        console.log("buzz")
+    }else{
+        console.log(x)
+    }
 
-console.log(x)
+    x++
+}
