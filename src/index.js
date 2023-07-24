9a6946f49f63bdc51ee4ee31c7ebb7face78c280
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-07-24 09:19:45 -0400

    Learned other way to write functions

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ function speak(){
     console.log("Greetings!")
 }
 
+const otherFunction = function(){
+    console.log('example text')
+}
+
+const otherFunction2 = () => {
+    console.log('This is another example')
+}
+
 body.addEventListener('click', speak)
