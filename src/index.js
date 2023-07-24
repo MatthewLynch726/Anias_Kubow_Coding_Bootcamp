1d9ee8c06d91809089d7e71d09bddacbb601268e
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-07-24 19:07:11 -0400

    creating reduce function

--- src/index.js
+++ src/index.js
@@ -16,8 +16,21 @@
 //using 'some' write a function that cheks an array for a 'falsey' value
 //ex. checkForFalsey([11, NaN, [], 'Angels']) => true
 
-function checkforFalesy(tempArray){
-    return tempArray.some(value => !value)
-}
+// function checkforFalesy(tempArray){
+//     return tempArray.some(value => !value)
+// }
 
-console.log(checkforFalesy([11, NaN, 'Angles']))
+// console.log(checkforFalesy([11, NaN, 'Angles']))
+
+//...............................
+
+// USING THE REDUCE FUNCTION
+//using reduce write a function that will return the total number of characters in an array of strings
+// const tempArray = ['Rabbit', 'Football', 'Cracking']
+// function getTotal(tempArray){
+//     const number = tempArray.reduce((totalCharacters, word) => {return word.length + totalCharacters} , 0)
+//     return number
+// }
+
+// console.log(getTotal(tempArray))
+
