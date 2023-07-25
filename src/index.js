fdf74ba44348c08bbd100bc18b2dc3fc83b19d2c
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-07-24 22:07:14 -0400

    Learned to use RemoveEventListener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const box = document.querySelector('.hexagon')
 
+let count = 10 
 function alertMe(){
-    box.
+    box.style.marginTop = count + 'px';
+    count += 10
+    if(count >= 200){
+        console.log("You have gone far enough!")
+        box.removeEventListener('click', alertMe)
+    }
 }
 
 box.addEventListener('click', alertMe)
